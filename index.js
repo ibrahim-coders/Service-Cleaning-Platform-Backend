45ce58be7cab38672f70642dcf01dad7416f6f87
MD IBRAHIM <ibrahim.webdev10@gmail.com>
2025-04-24 22:47:04 +0300

    server site error solve

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const cookieParser = require('cookie-parser');
 app.use(
   cors({
     origin: [
-      'http://localhost:5173'
-      // 'https://prodeact-service.web.app',
-      // 'https://prodeact-service.firebaseapp.com',
+     
+      'https://prodeact-service.web.app',
+      'https://prodeact-service.firebaseapp.com',
     ],
     credentials: true,
   })
@@ -20,9 +20,9 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-// const uri = `mongodb+srv://${process.env.SECRET_NAME}:${process.env.SECRET_PASS}@cluster0.whh17.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.SECRET_NAME}:${process.env.SECRET_PASS}@cluster0.whh17.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri ='mongodb://localhost:27017/Assignment-11' 
+// const uri ='mongodb://localhost:27017/Assignment-11' 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
